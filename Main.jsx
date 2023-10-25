7c44faea1df1c2a28406d52d7a7858c50ddf33d3
Ezio357 <136444915+Ezio357@users.noreply.github.com>
2023-10-25 17:12:57 +0300

    Create Main.jsx

---
+++ Main.jsx
@@ -0,0 +1,14 @@
+// файл с краткой информацией
+import React from 'react'
+import './Main.css';
+
+export default function Main() {
+  return (
+    <div className='main'>
+    <div className='mainInfo'>
+        <h1 className='h1'>Краткая информация</h1>
+        <p className='textMini'>Этот проект направлен на краткое знакомство с биологическими науками. Анатомия, психология, физиология, гигиена и медицине Написан он при помощи фреймворка React. На сайте есть несколько разделов, которые являются маршрутизацией, написанно при помощи SPA. В нем представлено 3 раздела. 1-основной, на нем содержиться краткая информация, которую Вы читаете сейчас. 2-науки, вот так как раз можно кратко прочитать подробнее об основных науках, изучающих человека, которые я перечислил ранее. И 3-Чат. В нем можно будет написать немколько сообщений в чат. На этом все. Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum at quae quam perferendis, suscipit est voluptas expedita impedit quisquam, natus corrupti tempore voluptatibus saepe excepturi? Repellat, minus! Eius, suscipit. Corporis nam reiciendis hic magni alias placeat veniam voluptate eius nihil, tenetur ducimus numquam soluta repellat? Facere beatae ipsam nemo asperiores iusto, repudiandae dignissimos libero temporibus. Excepturi velit consequatur sequi accusantium ab at aspernatur est illum error. Iure impedit beatae autem fugit consequatur magni excepturi soluta voluptatibus quaerat! Quae, suscipit aliquid. Dolorem animi eum exercitationem nulla nostrum, asperiores explicabo est soluta itaque nobis, quidem molestiae, consequuntur maiores reiciendis officiis consequatur doloremque.</p>
+    </div>
+    </div>
+  )
+}
