433e4553f177f7af857981807b04868a1d8d897a
Ezio357 <136444915+Ezio357@users.noreply.github.com>
2023-10-25 17:16:29 +0300

    Create Med.jsx

---
+++ Med.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styleSciences.css'
+
+export default function Med() {
+  return (
+    <div>
+
+      <div className="block1">
+        <img src="https://avatars.mds.yandex.net/i?id=2cb0cce0f0d48a1faaacb7e08a31384f988d2ea3-10303547-images-thumbs&n=13" alt="" />
+        <h1 className='h1'>Медицина</h1>
+        <p className='text'>Медицина — это наука, изучающая человека в здоровом и болезненном состоянии с целью укрепления его здоровья, охраны от болезней и лечения. Таким образом, в задачи медицинской науки входит не только лечение больных, но и укрепление здоровья здоровых.</p>
+      </div>
+
+    </div>
+  )
+}
