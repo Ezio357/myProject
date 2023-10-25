50d6dc74fbce68d01c1e3c588bc84c5eb2743b1e
Ezio357 <136444915+Ezio357@users.noreply.github.com>
2023-10-25 17:15:07 +0300

    Create Anatom.jsx

---
+++ Anatom.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Anatom() {
+  return (
+    <div>
+      <div className="block1">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Lateral_head_anatomy_detail.jpg/220px-Lateral_head_anatomy_detail.jpg" alt="" />
+        <h1 className='h1'>Aнатомия</h1>
+        <p className='text'>Анатомия, изучающей закономерности развития и строения тканей, а также с наукой о клетке, которая исследует закономерности развития, строения и деятельности отдельных клеток, из которых построены ткани и органы исследуемого макроорганизма. Взятые вместе анатомия, гистология, цитология и эмбриология (от греч. émbryon — зародыш) в совокупности представляют общую науку о форме, развитии и строении организма — морфологию.</p>
+      </div>
+    </div>
+  )
+}
